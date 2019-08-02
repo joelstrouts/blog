30ff9f0213afdf703cab82912fb189cdd42a906f
Joel Strouts <joelstrouts@gmail.com>
2019-08-02 22:00:36 +0100

    make consideration of canvas dims implicit in api

--- _includes/scripts/custom/my-fern-vanilla.js
+++ _includes/scripts/custom/my-fern-vanilla.js
@@ -7,6 +7,23 @@ const zip = (list1, list2) => list1.map((value, key) => [value, list2[key]]);
 const getComposition = (...functions) => (point) => {
   return functions.reduce((value, func) => func(value), point);
 }
+const override = (defaults, reconsiderations) => {
+  let returnValues = {};
+  if (!reconsiderations) {
+    for (index in Object.keys(defaults)) {
+      let key = Object.keys(defaults)[index];
+      returnValues[key] = defaults[key];
+    }
+    return defaults;
+  } else {
+    for (index in Object.keys(defaults)) {
+      let key = Object.keys(defaults)[index];
+      returnValues[key] =  
+        (reconsiderations[key]) ? reconsiderations[key] : defaults[key];
+    }
+    return returnValues;
+  }
+}
 
 /* LINEAR ALGEBRA
  * (VECTOR RELATED)
@@ -43,40 +60,38 @@ const cummulative = list => {
   let runningTotal = list[0];
   for (var i = 1; i < list.length; i++) {
     runningTotal += list[i];
-    returnList = Array.concat(returnList, runningTotal);
+    returnList = returnList.concat(runningTotal);
   }
   return returnList;
 }
+const constrain = (x, bounds) => Math.max(Math.min(x,bounds[1],bounds[0]));
 
 /* PROJECTION / DRAWING
  */
-const getCoordinateTransform = (inplex, outplex) => (point) => {
-  const rx = vMinus(inplex[1],  inplex[0]);
-  const ry = vMinus(inplex[2],  inplex[0]);
-  const rx_out = vMinus(outplex[1],  outplex[0]);
-  const ry_out = vMinus(outplex[2],  outplex[0]);
+const getCoordinateTransform = (from, to) => (point) => {
   return getComposition(
-    getTranslation(vScale(-1, inplex[0])),
-    getLinear(mInverse([rx, ry])),
-    getLinear([rx_out, ry_out]),
-    getTranslation(outplex[0])
+    getTranslation(vScale(-1, from.o)),
+    getLinear(mInverse([from.x, from.y])),
+    getLinear([to.x, to.y]),
+    getTranslation(to.o)
   )(point);
 }
 const paintPixel = ([x,y], imgData, rgba) => {
-  const index = 4 * (y * imgData.width + x);
-  imgData.data[index + 0] = rgba[0];
-  imgData.data[index + 1] = rgba[1];
-  imgData.data[index + 2] = rgba[2];
-  imgData.data[index + 3] = rgba[3];
+  if  (x > 0 
+    && x < imgData.width
+    && y > 0
+    && y < imgData.height) {
+    const index = 4 * (y * imgData.width + x);
+    imgData.data[index + 0] = rgba[0];
+    imgData.data[index + 1] = rgba[1];
+    imgData.data[index + 2] = rgba[2];
+    imgData.data[index + 3] = rgba[3];
+  }
 }
 const paintPixelBox = ([x,y], imgData, rgba, size) => {
   for (var i = - size; i < size; i++) {
     for (var j = - size; j < size; j++) {
-    const index = 4 * ((y + j) * imgData.width + (x + i));
-    imgData.data[index] = rgba[0];
-    imgData.data[index + 1] = rgba[1];
-    imgData.data[index + 2] = rgba[2];
-    imgData.data[index + 3] = rgba[3];
+      paintPixel([x + i, y + j], imgData, rgba);
     }
   }
 }
@@ -97,11 +112,11 @@ const drawLine = ([x1,y1], [x2,y2], imgData, rgb) => {
   let xpxl1 = xend;
   let ypxl1 = ipart(yend);
   if (steep) {
-    paintPixel([ypxl1, xpxl1],     imgData, Array.concat(rgb, rfpart(yend) * xgap * 255));
-    paintPixel([ypxl1 + 1, xpxl1], imgData, Array.concat(rgb,  fpart(yend) * xgap * 255));
+    paintPixel([ypxl1, xpxl1],     imgData, rgb.concat(rfpart(yend) * xgap * 255));
+    paintPixel([ypxl1 + 1, xpxl1], imgData, rgb.concat( fpart(yend) * xgap * 255));
   } else {
-    paintPixel([xpxl1, ypxl1],     imgData, Array.concat(rgb, rfpart(yend) * xgap * 255));
-    paintPixel([xpxl1, ypxl1 + 1], imgData, Array.concat(rgb,  fpart(yend) * xgap * 255));
+    paintPixel([xpxl1, ypxl1],     imgData, rgb.concat(rfpart(yend) * xgap * 255));
+    paintPixel([xpxl1, ypxl1 + 1], imgData, rgb.concat( fpart(yend) * xgap * 255));
   }
   let intery = yend + m;
 
@@ -112,24 +127,24 @@ const drawLine = ([x1,y1], [x2,y2], imgData, rgb) => {
   let xpxl2 = xend;
   let ypxl2 = ipart(yend);
   if (steep) {
-    paintPixel([ypxl2, xpxl2],     imgData, Array.concat(rgb, rfpart(yend) * xgap * 255));
-    paintPixel([ypxl2 + 1, xpxl2], imgData, Array.concat(rgb, fpart(yend) * xgap * 255));
+    paintPixel([ypxl2, xpxl2],     imgData, rgb.concat(rfpart(yend) * xgap * 255));
+    paintPixel([ypxl2 + 1, xpxl2], imgData, rgb.concat(fpart(yend) * xgap * 255));
   } else {
-    paintPixel([xpxl2, ypxl2],     imgData, Array.concat(rgb, rfpart(yend) * xgap * 255));
-    paintPixel([xpxl2, ypxl2 + 1], imgData, Array.concat(rgb,  fpart(yend) * xgap * 255));
+    paintPixel([xpxl2, ypxl2],     imgData, rgb.concat(rfpart(yend) * xgap * 255));
+    paintPixel([xpxl2, ypxl2 + 1], imgData, rgb.concat( fpart(yend) * xgap * 255));
   }
 
   // line inbetween
   if (steep) {
     for (var x = xpxl1 + 1; x < xpxl2; x++) {
-      paintPixel([ipart(intery), x],     imgData, Array.concat(rgb, rfpart(intery) * 255));
-      paintPixel([ipart(intery) + 1, x], imgData, Array.concat(rgb,  fpart(intery) * 255));
+      paintPixel([ipart(intery), x],     imgData, rgb.concat(rfpart(intery) * 255));
+      paintPixel([ipart(intery) + 1, x], imgData, rgb.concat( fpart(intery) * 255));
       intery = intery + m;
     }
   } else {
     for (var x = xpxl1 + 1; x < xpxl2; x++) {
-      paintPixel([x, ipart(intery)],     imgData, Array.concat(rgb, rfpart(intery) * 255));
-      paintPixel([x, ipart(intery) + 1], imgData, Array.concat(rgb,  fpart(intery) * 255));
+      paintPixel([x, ipart(intery)],     imgData, rgb.concat(rfpart(intery) * 255));
+      paintPixel([x, ipart(intery) + 1], imgData, rgb.concat( fpart(intery) * 255));
       intery = intery + m;
     }
   }
@@ -137,9 +152,15 @@ const drawLine = ([x1,y1], [x2,y2], imgData, rgb) => {
 const drawPolygon = (verts, imgData, rgba) => {
   let rgb = rgba.slice(0,3);
   verts = verts.concat([verts[0]]);
-  verts.map(p => { paintPixelBox(p, imgData, rgba, 4); return p});
   verts.reduce((a,b) => { drawLine(a,b,imgData,rgb); return b; });
+  verts.map(p => { paintPixelBox(p, imgData, rgba, 3); return p});
 }
+
+const paint = (canvas, fractalGetter) => {
+  Fractal.canvas = canvas;
+  canvas.getContext('2d').putImageData(fractalGetter(), 0, 0);
+}
+
 /************************/
 /* HELPER FUNCTIONS END */
 /************************/
@@ -147,7 +168,6 @@ const drawPolygon = (verts, imgData, rgba) => {
 /************************/
 /*    PRESETS           */
 /************************/
-
 const randomAffine = () => {
   let r = [0,0,0,0,0,0].map(() => Math.random() * 2 - 1);
   return getAffine([[r[0], r[1]], [r[2], r[3]]], [r[4], r[5]]);
@@ -156,80 +176,61 @@ const randomLinear = () => {
   let r = [0,0,0,0].map(() => Math.random() * 2 - 1);
   return getLinear([[r[0], r[1]], [r[2], r[3]]], [r[4], r[5]]);
 }
-
-const getFractal = (def, N, providedOptions) => {
-  const imageData = new ImageData(
-    def.canvasDimensions[0],
-    def.canvasDimensions[1]
-  );
-  let p = [0,0];
-  let options = {};
-  if (providedOptions) {
-    options = providedOptions;
-    if (!options.style)  { options.style = Fractal.defaultOptions.style; }
-    if (!options.color)  { options.color = Fractal.defaultOptions.color; }
-    if (!options.colors) { options.colors = Fractal.defaultOptions.colors; }
-    if (!options.bboxes) { options.bboxes = Fractal.defaultOptions.bboxes; }
-  } else {
-    options = Fractal.defaultOptions;
-  }
-  let thisColor;
-  if (options.color == 'last') {
-    Fractal.lastTransform = 0;
-    thisColor = () => options.colors[Fractal.lastTransform];
-  } else {
-    thisColor = () => options.colors[0];
-  }
-
-  if (options.bboxes) {
-    let [a,b,d] = def.referenceRegion; c = vAdd(b, vMinus(d, a));
-    let [p,q,r,s] = [a,b,c,d].map(p => def.getPixel(p));
-    drawPolygon([p,q,r,s], imageData, thisColor());
-    Fractal.lastTransform = 0;
-    def.transforms.map(f => {
-      [p,q,r,s] = [a,b,c,d].map(p => def.getPixel(f(p)));
-      drawPolygon([p,q,r,s], imageData, thisColor());
-      Fractal.lastTransform += 1;  
-    })
-  }
-  // MAIN LOOP
-  if(options.style == 'lines') {
-    // draw initial point before the main loop begins
-    let pixelOrigin = def.ctf(p).map(e => Math.floor(e));
-    paintPixelBox(pixelOrigin, imageData, Array.concat(thisColor(), 255), 4);
-  }
-  for (var i = 0; i < N; i++) {
-    let q = Fractal.applyTransform(def)(p);
-    let [a, b] = [p,q].map(v => def.ctf(v).map(e => Math.floor(e)));
-    if (options.style == 'dots') {
-      paintPixel(b, imageData, Array.concat(thisColor(), 255));
-    } else if (options.style == 'blobs') {
-      paintPixelBox(b, imageData, Array.concat(thisColor(), 255), 1);
-    } else if (options.style == 'lines') {
-      drawLine(a, b, imageData, thisColor());
-      paintPixelBox(b, imageData, Array.concat(thisColor(), 255), 3);
-    }
-    p = q;
-  }
-  return imageData;
+const scaleTransform = (f, amount) => p => {
+  return getComposition(f, getLinear([[amount,0],[0,amount]]))(p)
 }
+const getOutputRegion = (referenceRegion, canvas, options) => {
+  let outputRegion = { o: [0,0], x: [1,0], y: [0,1] };
+  switch (options.position) {
+    case 'centre':
+      let inX = vMod(referenceRegion.x), inY = vMod(referenceRegion.y),
+          canvX = canvas.width,          canvY = canvas.height,
+          outX,                          outY;
+      if (inY / inX > canvY / canvX) { // slack width
+        outY = canvY;
+        outX = Math.floor((inX / inY) * canvY);
+        outputRegion = { 
+          o: [
+            Math.floor((canvY - outY) / 2) + options.margin, 
+            outY - options.margin
+          ],
+          x: [outX - 2 * options.margin, 0],
+          y: [0, - outY + 2 * options.margin],
+        }
+      } else { // slack height
+        outX = canvX;
+        outY = Math.abs(Math.floor((inY / inX) * canvX));
+        outputRegion = { 
+          o: [
+            options.margin, 
+            outY + Math.floor(canvY - outY) / 2 - options.margin
+          ],
+          x: [outX - 2 * options.margin, 0],
+          y: [0, 2 * options.margin - outY],
+        }
+      }
+      break;
+  }
+  return outputRegion;
+}
+
 const Fractal = {
-  init: (def) => {
-    if (def.init) {
-      def.init(); 
-    }
+  init: (def, options) => {
+    if (def.init) { def.init(); }
     def.ctf = getCoordinateTransform(
-      def.referenceRegion, [
-        [def.margin, def.canvasDimensions[1] - def.margin], 
-        [def.margin, def.margin], 
-        [def.canvasDimensions[0] - def.margin, def.canvasDimensions[1] - def.margin]
-    ]);
+      def.referenceRegion, 
+      getOutputRegion(def.referenceRegion, Fractal.canvas, options)
+    );
+    if (def.probabilities === 'equal') {
+      def.probabilities = Array(def.transforms.length).fill(1/def.transforms.length);
+    }
     def.getPixel = p => def.ctf(p).map(e => Math.floor(e));
     def.probBins = cummulative(def.probabilities);
   },
-  get: (def, n, drawingMethod, coloringMethod) => {
-    Fractal.init(def);
-    return getFractal(def, n, drawingMethod, coloringMethod);
+  get: (def, n, options) => () => {
+    options = override(Fractal.defaultOptions, options);
+    Fractal.init(def, options);
+    return getFractal(def, n, options);
   },
   applyTransform: def => p => {
     let rand = Math.random();
@@ -239,6 +240,7 @@ const Fractal = {
     return def.transforms[choice](p);
   },
   defaultOptions: {
+    runnable: false,
     style: 'dots',
     color: 'uniform',
     colors: [
@@ -247,16 +249,69 @@ const Fractal = {
       [200, 110, 0],
       [210, 0, 0],
       [0, 0, 200],
+      [0, 110, 200],
       [40, 40, 40],
     ],
     bboxes: false,
+    blobsSize: 1,
+    initialPoint: [0,0],
+    position: 'centre',
+    margin: 40,
+  },
+}
+const getFractal = (def, N, options) => {
+  const imageData = new ImageData(Fractal.canvas.width, Fractal.canvas.height);
+  let thisColor; if (options.color == 'last') {
+    Fractal.lastTransform = 0;
+    thisColor = () => options.colors[Fractal.lastTransform];
+  } else {
+    thisColor = () => options.colors[0];
   }
+  let p = options.initialPoint;
+  if (options.bboxes) {
+    let [a,b,c,d] = [
+      def.referenceRegion.o,
+      vAdd(def.referenceRegion.o, def.referenceRegion.x),
+      vAdd(def.referenceRegion.o, def.referenceRegion.x, def.referenceRegion.y),
+      vAdd(def.referenceRegion.o, def.referenceRegion.y)
+    ];
+    let [p,q,r,s] = [a,b,c,d].map(p => def.getPixel(p));
+    drawPolygon([p,q,r,s], imageData, [0,0,0,255]);
+    Fractal.lastTransform = 0;
+    def.transforms.map(f => {
+      [p,q,r,s] = [a,b,c,d].map(p => def.getPixel(f(p)));
+      drawPolygon([p,q,r,s], imageData, thisColor().concat(255));
+      Fractal.lastTransform += 1;  
+    })
+  }
+  // MAIN LOOP
+  if(options.style == 'lines') {
+    // draw initial point before the main loop begins
+    let pixelOrigin = def.ctf(p).map(e => Math.floor(e));
+    paintPixelBox(pixelOrigin, imageData, thisColor().concat(255), options.blobsSize);
+  }
+  for (var i = 0; i < N; i++) {
+    let q = Fractal.applyTransform(def)(p);
+    let [a, b] = [p,q].map(v => def.ctf(v).map(e => Math.floor(e)));
+    if (options.style == 'dots') {
+      paintPixel(b, imageData, thisColor().concat(255));
+    } else if (options.style == 'blobs') {
+      paintPixelBox(b, imageData, thisColor().concat(255), options.blobsSize);
+    } else if (options.style == 'lines') {
+      drawLine(a, b, imageData, thisColor());
+      paintPixelBox(b, imageData, thisColor().concat(255), options.blobsSize);
+    }
+    p = q;
+  }
+  return imageData;
 }
 
 const barnsleyFern = {
-  margin: 40,
-  canvasDimensions: [373, 752],
-  referenceRegion: [[-2.182,0],[-2.182,9.9983],[2.6558,0]],
+  referenceRegion: {
+    o: [-2.4,-0.5],
+    x: [5.5 ,0 ] ,
+    y: [0   ,11],
+  },
   transforms: [
     getLinear([[ 0.00, 0.00],[ 0.00, 0.16]]),
     getAffine([[ 0.85,-0.04],[ 0.04, 0.85]], [0.00,1.60]),
@@ -269,57 +324,78 @@ const barnsleyFern = {
     0.07,
     0.07,
   ],
-}
+};
 const binaryTree = {
-  margin: 40,
-  canvasDimensions: [373, 752],
-  referenceRegion: [[-0.5,0],[-0.5,1],[0.5,0]],
+  referenceRegion: {
+    o: [-0.5,0  ],
+    x: [1   ,0  ] ,
+    y: [0   ,0.9],
+  },
   transforms: [
-    getLinear([[ 0.01, 0.00],[ 0.00, 0.45]]),
-    getAffine([[-0.01,-0.04],[ 0.00,-0.45]], [0.00, 0.40]),
+    getLinear([[ 0, 0.00],[ 0.00, 0.45]]),
     getAffine([[ 0.42, 0.42],[-0.42, 0.42]], [0.00, 0.40]),
     getAffine([[ 0.42,-0.42],[ 0.42, 0.42]], [0.00, 0.40]),
   ],
-  probabilities: Array(4).fill(1/4),
-}
+  probabilities: Array(3).fill(1/3),
+};
 const mapleLeaf = {
-  margin: 40,
-  canvasDimensions: [373, 752],
-  referenceRegion: [[-3,-4],[-3,5],[3,-4]],
+  referenceRegion: {
+    o: [-3.5,-3.5],
+    x: [7,0] ,
+    y: [0,7],
+  },
   transforms: [
-    getAffine([[-0.30, 0.00],[ 0.01,-0.51]], [-0.08,-1.31]),
-    getAffine([[ 0.43,-0.45],[ 0.52, 0.50]], [ 1.49,-0.75]),
-    getAffine([[ 0.45, 0.47],[-0.49, 0.47]], [-1.62,-0.74]),
-    getAffine([[ 0.49, 0.00],[ 0.00, 0.51]], [ 0.02, 1.62]),
+    getComposition(getLinear([[0.5,0],[0,0.65]]), getTranslation([0,1.2])),
+    getAffine([[ 0.45,-0.45],[ 0.45, 0.45]], [ 1.62,-0.65]),
+    getAffine([[ 0.45, 0.45],[-0.45, 0.45]], [-1.62,-0.65]),
+    getLinear([[0,0],[0,-1.1]]),
   ],
-  probabilities: Array(4).fill(1/4),
-}
+  probabilities: [0.31,0.31,0.31,0.07]
+};
 const sandDollar = {
-  margin: 40,
-  canvasDimensions: [373, 752],
-  referenceRegion: [[0,0],[0,1],[1,0]],
+  referenceRegion: {
+    o: [0,0],
+    x: [1,0] ,
+    y: [0,1],
+  },
   transforms: [
-    getAffine([[ 0.382, 0.0100],[ 0.0000, 0.382]], [0.3090, 0.5700]),
+    getAffine([[ 0.382, 0.0000],[ 0.0000, 0.382]], [0.3090, 0.5710]),
     getAffine([[ 0.118, 0.3630],[-0.3630, 0.118]], [0.3633, 0.3306]),
     getAffine([[ 0.118,-0.3630],[ 0.3630, 0.118]], [0.5187, 0.6940]),
     getAffine([[-0.309, 0.2245],[-0.2245,-0.309]], [0.6070, 0.3090]),
     getAffine([[-0.309,-0.2245],[ 0.2245,-0.309]], [0.7016, 0.5335]),
     getAffine([[ 0.382, 0.0000],[ 0.0000,-0.382]], [0.3090, 0.6770]),
   ],
-  probabilities: Array(6).fill(1/6),
-}
+  probabilities: 'equal',
+};
+const serpinski = {
+  referenceRegion: {
+    o: [0,0],
+    x: [2,0],
+    y: [0,2],
+  },
+  transforms: [
+    getLinear([[1/2, 0],[0,1/2]]),
+    getAffine([[1/2, 0],[0,1/2]], [1, 0]),
+    getAffine([[1/2, 0],[0,1/2]], [0, 1]),
+  ],
+  probabilities: Array(3).fill(1/3),
+};
 const random = {
-  margin: 60,
-  canvasDimensions: [373, 752],
-  referenceRegion: [[-1,-1],[-1,1],[1,-1]],
-  num: 4,
-  transforms: [], // defined on function call
-  probabilities: [], // ditto
+  scale: 1,
+  order: 4,
+  referenceRegion: {
+    o: [-0.5,0],
+    x: [1   ,0] ,
+    y: [0   ,1],
+  },
+  transforms: [], // defined on object initialization
+  probabilities: [], 
   init: () => {
     random.transforms = [];
-    for (var i = 0; i < random.num; i++) {
-      random.transforms = Array.concat(random.transforms, randomAffine());
+    for (var i = 0; i < random.order; i++) {
+      random.transforms = random.transforms.concat(scaleTransform(randomAffine(), random.scale))
     }
-    random.probabilities = Array(random.num).fill(1/random.num);
+    random.probabilities = Array(random.order).fill(1/random.order);
   },
-}
+};
